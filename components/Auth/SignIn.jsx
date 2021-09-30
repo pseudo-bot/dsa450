9b67c3483691dc4f775374a845dbf4d4a0854113
pseudobot <shivamx6174@gmail.com>
2021-09-30 09:56:13 +0530

    alignment

--- components/Auth/SignIn.jsx
+++ components/Auth/SignIn.jsx
@@ -1,19 +1,19 @@
 const SignInCard = () => {
 	return (
 		<div
-			className={`mx-auto h-[20rem] max-w-sm rounded-2xl shadow-lg flex flex-col p-6 
+			className={`mx-auto h-[20rem] max-w-sm rounded-2xl shadow-lg flex flex-col 
       items-center justify-center relative bg-gray-200
       after:absolute after:w-full after:h-[150%] after:rotate-[15deg] after:bg-green-400 after:-left-1/2 
       tracking-wider overflow-hidden`}
 		>
 			<div className="text-4xl my-5 text-green-900 uppercase text-center poppins z-10">Login</div>
-			<div className="h-10 shadow-lg my-5 w-4/5 bg-white rounded-full flex items-center justify-center relative cursor-pointer poppins tracking-wider z-10 text-sm sm:text-base text-gray-700">
+			<div className="h-10 shadow-lg my-5 bg-white rounded-full flex items-center justify-between px-1 relative cursor-pointer poppins tracking-wider z-10 text-sm sm:text-base text-gray-700">
 				<img
 					src="/images/google.png"
 					alt="Sign in with google"
-					className={`block h-3/5 mx-2`}
+					className={`block h-3/5 mx-3`}
 				/>
-				<span className="mx-2">Sign in with google</span>
+				<span className="mx-3">Sign in with google</span>
 			</div>
 		</div>
 	);
--- components/Content/Card.jsx
+++ components/Content/Card.jsx
@@ -5,7 +5,7 @@ export default function Cards({ topic, total, solved }) {
 		<Link href={`/${topic}/problems`}>
 			<div data-aos="fade-right" className="h-64 dark:bg-gray-800 dark:text-gray-300 bg-indigo-100 flex flex-col group justify-around p-5 w-full rounded-xl shadow-md my-4 sm:max-w-md sm:mx-8 relative cursor-pointer">
 				<div className="sm:text-3xl text-2xl text-gray-700 dark:text-gray-300 uppercase font-semibold m-2 mb-4">{topic}</div>
-						<div className="m-2 sm:text-6xl text-4xl left-1/2 -translate-x-1/2 absolute font-extrabold text-indigo-200 dark:text-gray-600 z-0">{total}</div>
+						<div className="m-2 sm:text-6xl text-4xl sm:translate-x-1/2 absolute font-extrabold text-indigo-200 dark:text-gray-600 z-0 right-16 sm:right-1/2">{total}</div>
 						<div className="m-2 sm:text-base text-sm">Solved: {solved}</div>
 						<div className="m-2 sm:text-base text-sm">Remaining: {total - solved}</div>
 						<div className="m-2 mt-4 rounded bg-indigo-400 dark:bg-gray-600 sm:absolute right-8 text-gray-50 w-max p-3 shadow-lg transition-transform duration-200 sm:group-hover:scale-110">Start <span className="jetbrains">-{">"}</span></div>
